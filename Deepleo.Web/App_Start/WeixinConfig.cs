f2a6e89715bede952e6ce52b062e6c77a77286f4
yxbbbs <yxbbbs@163.com>
2015-02-04 09:50:35 +0800

    为web.config配置项添加注释

--- Deepleo.Web/App_Start/WeixinConfig.cs
+++ Deepleo.Web/App_Start/WeixinConfig.cs
@@ -17,6 +17,7 @@ namespace Deepleo.Web
         public static string device_info { private set; get; }
         public static string spbill_create_ip { private set; get; }
         public static TokenHelper TokenHelper { private set; get; }
+
         public static void Register()
         {
 
@@ -29,6 +30,7 @@ namespace Deepleo.Web
             device_info = System.Configuration.ConfigurationManager.AppSettings["device_info"];
             spbill_create_ip = System.Configuration.ConfigurationManager.AppSettings["spbill_create_ip"];
             var openJSSDK = int.Parse(System.Configuration.ConfigurationManager.AppSettings["OpenJSSDK"]) > 0;
+
             TokenHelper = new TokenHelper(6000, AppID, AppSecret, openJSSDK);
             TokenHelper.Run();
         }
